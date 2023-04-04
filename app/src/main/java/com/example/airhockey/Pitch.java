7c24c86902438b01c43b54b769098af48c88e002
stbaciva <stbaciva@bx.fallmerayer.it>
2023-04-04 16:34:47 +0200

    task (pitch): add move funktion player1

--- app/src/main/java/com/example/airhockey/Pitch.java
+++ app/src/main/java/com/example/airhockey/Pitch.java
@@ -1,11 +1,58 @@
 package com.example.airhockey;
+
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class Pitch extends AppCompatActivity {
+    private ImageView player1;
+    private float xCoOrdinate, yCoOrdinate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pitch);
+
+        player1 = findViewById(R.id.imageViewPlayer1);
+
+        player1.setOnTouchListener(new View.OnTouchListener() {
+            ConstraintLayout layout = findViewById(R.id.pitchlayout);
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getActionMasked()) {
+                    case MotionEvent.ACTION_DOWN:
+                        xCoOrdinate = view.getX() - event.getRawX();
+                        yCoOrdinate = view.getY() - event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        float newX = event.getRawX() + xCoOrdinate;
+                        float newY = event.getRawY() + yCoOrdinate;
+
+                        // Überprüfe, ob die ImageView das Layout überschreitet
+                        if (newX < 0) {
+                            newX = 0;
+                        } else if (newX > layout.getWidth() - view.getWidth()) {
+                            newX = layout.getWidth() - view.getWidth();
+                        }
+                        if (newY < 0) {
+                            newY = 0;
+                        } else if (newY > this.layout.getHeight() - view.getHeight()) {
+                            newY = layout.getHeight() - view.getHeight();
+                        }
+
+                        view.animate().x(newX).y(newY).setDuration(0).start();
+                        break;
+                    default:
+                        return false;
+                }
+                return true;
+            }
+        });
     }
 }
