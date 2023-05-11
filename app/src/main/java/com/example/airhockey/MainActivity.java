e8d8665d8dc293bd9860f028e97253972a5472a8
Ulp-aka-MenschlicherCompiler <matthiasulpmer@gmail.com>
2023-05-11 22:41:03 +0200

    task (multiplayer): add device-search

--- app/src/main/java/com/example/airhockey/MainActivity.java
+++ app/src/main/java/com/example/airhockey/MainActivity.java
@@ -5,28 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
 import android.os.Bundle;
+
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.CompositePageTransformer;
 import androidx.viewpager2.widget.MarginPageTransformer;
 import androidx.viewpager2.widget.ViewPager2;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 
 import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private ViewPager2 viewPager2;
@@ -36,6 +37,21 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonSinglePlayer, buttonMultiPlayer, buttonBuy;
     private Animation scaleUp, scaleDown;
     private Database db;
+    private Handler handler;
+    private Runnable dotRunnable;
+    private int dotCount = 0;
+    private boolean isSearching = false;
+    private NsdManager mNsdManager;
+    private NsdManager.DiscoveryListener mDiscoveryListener;
+    private NsdManager.ResolveListener mResolveListener;
+    private String mServiceName = "MyService";
+    private String mServiceType = "_http._tcp.";
+    private boolean mIsServer = false;
+    private ServerSocket mServerSocket;
+    private Thread mServerThread;
+    private boolean mIsConnected = false;
+    private boolean mSearchInProgress = false;
+    private NsdManager.RegistrationListener mRegistrationListener;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -66,6 +82,95 @@ public class MainActivity extends AppCompatActivity {
         scaleUp = AnimationUtils.loadAnimation(this, R.anim.scale_up);
         scaleDown = AnimationUtils.loadAnimation(this, R.anim.scale_down);
 
+        mNsdManager = (NsdManager) getSystemService(Context.NSD_SERVICE);
+
+        mDiscoveryListener = new NsdManager.DiscoveryListener() {
+            @Override
+            public void onDiscoveryStarted(String serviceType) {
+                Log.d("NSD", "Discovery started");
+                mSearchInProgress = true;
+            }
+
+            @Override
+            public void onDiscoveryStopped(String serviceType) {
+                Log.d("NSD", "Discovery stopped");
+                mSearchInProgress = false;
+            }
+
+            @Override
+            public void onServiceFound(NsdServiceInfo serviceInfo) {
+                Log.d("NSD", "Service found: " + serviceInfo);
+                if (serviceInfo.getServiceType().equals(mServiceType) && !mIsConnected) {
+                    connectToService(serviceInfo);
+                }
+            }
+
+            @Override
+            public void onServiceLost(NsdServiceInfo serviceInfo) {
+                Log.d("NSD", "Service lost: " + serviceInfo);
+            }
+
+            @Override
+            public void onStartDiscoveryFailed(String serviceType, int errorCode) {
+                Log.e("NSD", "Discovery failed: " + errorCode);
+            }
+
+            @Override
+            public void onStopDiscoveryFailed(String serviceType, int errorCode) {
+                Log.e("NSD", "Stop discovery failed: " + errorCode);
+            }
+        };
+
+        // Initialize mRegistrationListener
+        mRegistrationListener = new NsdManager.RegistrationListener() {
+            @Override
+            public void onRegistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+                System.out.println("registration failure");
+            }
+
+            @Override
+            public void onUnregistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+                System.out.println("unregistration failure");
+            }
+
+            @Override
+            public void onServiceRegistered(NsdServiceInfo serviceInfo) {
+                System.out.println("successful registration");
+            }
+
+            @Override
+            public void onServiceUnregistered(NsdServiceInfo serviceInfo) {
+                System.out.println("successful unregistration");
+            }
+        };
+
+
+        mResolveListener = new NsdManager.ResolveListener() {
+            @Override
+            public void onResolveFailed(NsdServiceInfo serviceInfo, int errorCode) {
+                Log.e("NSD", "Resolve failed: " + errorCode);
+            }
+
+            @Override
+            public void onServiceResolved(NsdServiceInfo serviceInfo) {
+                Log.d("NSD", "Service resolved: " + serviceInfo);
+            }
+        };
+
+        mResolveListener = new NsdManager.ResolveListener() {
+            @Override
+            public void onResolveFailed(NsdServiceInfo serviceInfo, int errorCode) {
+                Log.e("NSD", "Resolve failed: " + errorCode);
+            }
+
+            @Override
+            public void onServiceResolved(NsdServiceInfo serviceInfo) {
+                Log.d("NSD", "Service resolved: " + serviceInfo);
+            }
+        };
+
+        mNsdManager = null;
+        //mNsdManager.registerService(serviceInfo, NsdManager.PROTOCOL_DNS_SD, mRegistrationListener);
         buttonSinglePlayer.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -79,19 +184,57 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+        // Handler initialisieren, um Punkte zu animieren
+        handler = new Handler(Looper.getMainLooper());
+        dotRunnable = new Runnable() {
+            @Override
+            public void run() {
+                if (dotCount >= 4) {
+                    dotCount = 0;
+                }
+                String dots = "";
+                for (int i = 0; i < dotCount; i++) {
+                    dots += ".";
+                }
+                buttonMultiPlayer.setText("Player search began" + dots);
+                dotCount++;
+                handler.postDelayed(this, 500);
+            }
+        };
 
         buttonMultiPlayer.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 if (motionEvent.getAction()== MotionEvent.ACTION_DOWN) {
                     buttonMultiPlayer.startAnimation(scaleUp);
+                    if (!isSearching) {
+                        isSearching = true;
+                        // Animation starten
+                        startAnimation();
+
+                    } else {
+                        // Animation stoppen
+                        isSearching = false;
+                        stopAnimation();
+                    }
+                    if (mIsConnected) {
+                        // Eine Verbindung besteht bereits, also können wir nichts tun
+                        return false;
+                    }
+
+                    if (mDiscoveryListener == null) {
+                        // Wenn der Listener null ist, dann wurde die Suche noch nicht gestartet
+                        discoverServices();
+                    } else {
+                        // Wenn der Listener nicht null ist, dann läuft die Suche gerade und wir müssen sie beenden
+                        stopSearch();
+                    }
                 } else if (motionEvent.getAction()== MotionEvent.ACTION_UP) {
                     buttonMultiPlayer.startAnimation(scaleDown);
                 }
                 return true;
             }
         });
-
     }
 
     private void goToGame(View view) {
@@ -122,8 +265,6 @@ public class MainActivity extends AppCompatActivity {
         imageList.add(R.drawable.skin4);
         imageList.add(R.drawable.skin5);
         imageList.add(R.drawable.skin6);
-        // imageList.add(R.drawable.skin7);
-        // imageList.add(R.drawable.skin8);
 
         adapter = new ImageAdapter(imageList, viewPager2, db, this, coinTextView);
 
@@ -157,7 +298,6 @@ public class MainActivity extends AppCompatActivity {
                     if(xDiff > 500) {
                         viewPager2.setCurrentItem(current - 1);
                     } else {
-                        System.out.println("Hallo: " + (current+1));
                         viewPager2.setCurrentItem(current + 1);
                     }
                 }
@@ -166,37 +306,86 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.dispatchTouchEvent(ev);
     }
+    private void startAnimation() {
+        dotCount = 0;
+        buttonMultiPlayer.setText("Player search began");
+        handler.post(dotRunnable);
+    }
 
-    /*
-    public void saveImagesToInternalStorage() {
-        SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
-        boolean imagesSaved = sharedPreferences.getBoolean("imagesSaved", false);
+    private void stopAnimation() {
+        handler.removeCallbacks(dotRunnable);
+        buttonMultiPlayer.setText("Multiplayer");
+    }
 
-        if (!imagesSaved) {
+    private void stopSearch() {
+        if (mNsdManager != null) {
+            mNsdManager.stopServiceDiscovery(mDiscoveryListener);
+            mDiscoveryListener = null;
+        }
+
+        if (mServerThread != null) {
+            mServerThread.interrupt();
+            mServerThread = null;
+        }
+
+        if (mServerSocket != null) {
             try {
-                String[] imageNames = {"skin1.png", "skin2.png", "skin3.png", "skin4.png", "skin5.png", "skin6.png", "skin7.png", "skin8.png"};
-
-                for (String imageName : imageNames) {
-                    int resId = getResources().getIdentifier(imageName, "drawable", getPackageName());
-                    Bitmap bitmap = BitmapFactory.decodeResource(getResources(), resId);
-
-                    FileOutputStream outputStream = openFileOutput(imageName, Context.MODE_PRIVATE);
-                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
-                    outputStream.flush();
-                    outputStream.close();
-                }
-
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putBoolean("imagesSaved", true);
-                editor.apply();
+                mServerSocket.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            mServerSocket = null;
         }
-    }*/
+
+        if (mIsServer) {
+            mNsdManager.unregisterService(mRegistrationListener);
+            mRegistrationListener = null;
+            mIsServer = false;
+        }
+
+        mIsConnected = false;
+    }
+
+    private void connectToService(NsdServiceInfo serviceInfo) {
+        // Perform the connection on a background thread to avoid blocking the UI thread
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Socket socket = new Socket(serviceInfo.getHost(), serviceInfo.getPort());
+                    // Connection established, do something with the socket
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void discoverServices() {
+        mNsdManager.discoverServices(mServiceType, NsdManager.PROTOCOL_DNS_SD, mDiscoveryListener);
+        // Wenn nach 5 Sekunden kein Gerät gefunden wird, selbst zum Server werden
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (!mIsServer) {
+                    registerService();
+                }
+            }
+        }, 5000); // Verzögerung von 5 Sekunden
+    }
+
+    private void registerService() {
+        NsdServiceInfo serviceInfo = new NsdServiceInfo();
+        serviceInfo.setServiceName(mServiceName);
+        serviceInfo.setServiceType(mServiceType);
+        serviceInfo.setPort(1234);
+
+        mNsdManager.registerService(serviceInfo, NsdManager.PROTOCOL_DNS_SD, mRegistrationListener);
+    }
     @Override
     protected void onDestroy() {
         super.onDestroy();
         db.close();
+        mNsdManager.stopServiceDiscovery(mDiscoveryListener);
     }
 }
